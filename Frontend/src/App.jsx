7fc89b41fc480beb807feb6ea93b64b5215ca46c
Adamin88 <32130262+Adamin88@users.noreply.github.com>
2024-09-27 05:32:15 +0000

    setup react

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl px-2 rounded-lg bg-slate-300 w-fit justify-center mx-auto">Hello Adam !</h1>
+    </>
+  )
+}
+
+export default App
